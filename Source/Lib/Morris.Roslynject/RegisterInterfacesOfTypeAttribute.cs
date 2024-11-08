508d2bc994e1b4a1ae5cfdf2e806454bb9a9671a
Peter Morris <mrpmorris@gmail.com>
2024-11-08 12:06:35 +0000

    2 more attributes

---
+++ Source/Lib/Morris.Roslynject/RegisterInterfacesOfTypeAttribute.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Morris.Roslynject;
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+public class RegisterInterfacesOfTypeAttribute : Attribute
+{
+    public Type BaseInterface { get; set; }
+    public ServiceLifetime ServiceLifetime { get; set; }
+    public InterfaceAs As { get; set; }
+
+    [StringSyntax(StringSyntaxAttribute.Regex)]
+    public string? InterfaceRegex { get; set; }
+
+    public RegisterInterfacesOfTypeAttribute(
+        Type baseInterface,
+        ServiceLifetime serviceLifetime,
+        InterfaceAs @as)
+    {
+        BaseInterface = baseInterface ?? throw new ArgumentNullException(nameof(baseInterface));
+        ServiceLifetime = serviceLifetime;
+        As = @as;
+    }
+}
---
+++ Source/Lib/Morris.Roslynject/RegisterInterfacesOnClassesDescendedFromAttribute.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Morris.Roslynject;
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+public class RegisterInterfacesOnClassesDescendedFromAttribute : Attribute
+{
+    public Type BaseClass { get; set; }
+    public ServiceLifetime ServiceLifetime { get; set; }
+
+    [StringSyntax(StringSyntaxAttribute.Regex)]
+    public string? InterfaceRegex { get; set; }
+
+    public RegisterInterfacesOnClassesDescendedFromAttribute(
+        Type baseClass,
+        ServiceLifetime serviceLifetime)
+    {
+        BaseClass = baseClass ?? throw new ArgumentNullException(nameof(baseClass));
+        ServiceLifetime = serviceLifetime;
+    }
+}
