9bf3b40478e021ff4b6a347886898564dde0ce00
Peter Morris <mrpmorris@gmail.com>
2024-11-12 17:56:30 +0000

    Refactor

--- Source/Lib/Morris.RoslynjectTests/RegisterClassesDescendedFromAttributeTests/NonGenericCriteria/WhenRegisteringDescendantClasses.cs
+++ Source/Lib/Morris.RoslynjectTests/RegisterClassesDescendedFromAttributeTests/NonGenericCriteria/WhenRegisteringDescendantClasses.cs
@@ -1,4 +1,4 @@
-﻿namespace Morris.RoslynjectTests.RegisterClassesDescendedFromAttributeTests;
+﻿namespace Morris.RoslynjectTests.RegisterClassesDescendedFromAttributeTests.NonGenericCriteria;
 
 [TestClass]
 public class WhenRegisteringDescendantClasses
@@ -22,7 +22,7 @@ public class WhenRegisteringDescendantClasses
 			public class Child2 : BaseClass {}
 			public class Child1Child1 : Child1 {}
 			""";
-		
+
 		const string expectedGeneratedCode =
 			"""
 			using Microsoft.Extensions.DependencyInjection;
@@ -39,6 +39,57 @@ public class WhenRegisteringDescendantClasses
 						services.AddScoped(typeof(global::MyNamespace.Child1));
 						services.AddScoped(typeof(global::MyNamespace.Child2));
 						services.AddScoped(typeof(global::MyNamespace.Child1Child1));
+
+						AfterRegister(services);
+					}
+				}
+			}
+
+			""";
+
+		SourceGeneratorExecutor.
+			AssertGeneratedCodeMatches(
+				sourceCode: sourceCode,
+				expectedGeneratedCode: expectedGeneratedCode
+			);
+	}
+
+	[TestMethod]
+	public void ThenRegistersClosedGenericClasses()
+	{
+		const string sourceCode =
+			"""
+			using Microsoft.Extensions.DependencyInjection;
+			using Morris.Roslynject;
+			namespace MyNamespace;
+			
+			[RegisterClassesDescendedFrom(typeof(BaseClass), ServiceLifetime.Scoped, RegisterClassAs.DescendantClass)]
+			internal class MyModule : RoslynjectModule
+			{
+			}
+			
+			public class BaseClass {}
+			public class Child1<T> : BaseClass {}
+			public class Child2<T> : BaseClass {}
+			public class Child1Child1 : Child1<int> {}
+			public class Child1Child1Child1 : Child1Child1 {}
+			""";
+
+		const string expectedGeneratedCode =
+			"""
+			using Microsoft.Extensions.DependencyInjection;
+
+			namespace MyNamespace
+			{
+				partial class MyModule
+				{
+					static partial void AfterRegister(IServiceCollection services);
+
+					public static void Register(IServiceCollection services)
+					{
+						// RegisterClassesDescendedFrom(typeof(BaseClass), ServiceLifetime.Scoped, RegisterClassAs.DescendantClass)
+						services.AddScoped(typeof(global::MyNamespace.Child1Child1));
+						services.AddScoped(typeof(global::MyNamespace.Child1Child1Child1));
 
 						AfterRegister(services);
 					}
