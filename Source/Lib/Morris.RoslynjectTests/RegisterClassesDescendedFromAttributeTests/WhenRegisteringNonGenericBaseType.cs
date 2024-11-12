b30c78acc46faca27c2f77978065b88699ce5225
Peter Morris <mrpmorris@gmail.com>
2024-11-12 16:51:10 +0000

    SourceGeneratorExecutor

--- Source/Lib/Morris.RoslynjectTests/RegisterClassesDescendedFromAttributeTests/WhenRegisteringNonGenericBaseType.cs
+++ Source/Lib/Morris.RoslynjectTests/RegisterClassesDescendedFromAttributeTests/WhenRegisteringNonGenericBaseType.cs
@@ -2,7 +2,6 @@
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.Extensions.DependencyInjection;
 using Morris.Roslynject.Generator;
-using static Basic.Reference.Assemblies.Net80;
 
 namespace Morris.RoslynjectTests.RegisterClassesDescendedFromAttributeTests;
 
@@ -12,52 +11,24 @@ public class WhenRegisteringNonGenericBaseType
 	[TestMethod]
 	public void ThenRegistersBaseClassForEachDescendantClass()
 	{
-		var syntaxTree = CSharpSyntaxTree.ParseText(
-			$$$"""
-			using Microsoft.Extensions.DependencyInjection;
-			using Morris.Roslynject;
-			namespace MyNamespace;
+		const string sourceCode =
+"""
+using Microsoft.Extensions.DependencyInjection;
+using Morris.Roslynject;
+namespace MyNamespace;
 
-			[RegisterClassesDescendedFrom(typeof(BaseClass), ServiceLifetime.Scoped, ClassRegistration.BaseClass)]
-			internal class MyModule : RoslynjectModule
-			{
-			}
+[RegisterClassesDescendedFrom(typeof(BaseClass), ServiceLifetime.Scoped, ClassRegistration.BaseClass)]
+internal class MyModule : RoslynjectModule
+{
+}
 
-			public class BaseClass {}
-			public class Child1 : BaseClass {}
-			public class Child2 : BaseClass {}
-			public class Child1Child1 : Child1 {}
-			"""
-		);
+public class BaseClass {}
+public class Child1 : BaseClass {}
+public class Child2 : BaseClass {}
+public class Child1Child1 : Child1 {}
+""";
 
-		var morrisRoslynjectAssembly = typeof(Morris.Roslynject.RegisterClassesDescendedFromAttribute).Assembly;
-		var morrisRoslynjectMetadataReference = MetadataReference.CreateFromFile(morrisRoslynjectAssembly.Location);
-
-		var injectionAssembly = typeof(ServiceLifetime).Assembly;
-		var injectionMetadataReference = MetadataReference.CreateFromFile(injectionAssembly.Location);
-
-		var compilation = CSharpCompilation.Create(
-			assemblyName: "Test",
-			syntaxTrees: [syntaxTree],
-			references: Basic.Reference.Assemblies.Net80.References
-				.All
-				.Union([morrisRoslynjectMetadataReference, injectionMetadataReference]),
-			options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary)
-		);
-
-		var subject = new SourceGenerator();
-		ISourceGenerator sourceGenerator = subject.AsSourceGenerator();
-		var driver = CSharpGeneratorDriver
-			.Create(sourceGenerator)
-			.RunGenerators(compilation);
-
-		GeneratorDriverRunResult runResult = driver.GetRunResult();
-		GeneratorRunResult result = runResult.Results.Single();
-
-		GeneratedSourceResult generatedSource = result.GeneratedSources.Single();
-		Assert.AreEqual("Morris.Roslynject.g.cs", generatedSource.HintName);
-		string generatedCode = generatedSource.SyntaxTree.ToString().Replace("\r", "");
-		string expectedGeneratedCode =
+		const string expectedGeneratedCode =
 """
 using Microsoft.Extensions.DependencyInjection;
 
@@ -79,9 +50,13 @@ namespace MyNamespace
     }
 }
 
-""".Replace("\r", "");
+""";
 
-		Assert.AreEqual(expectedGeneratedCode, generatedCode);
+		SourceGeneratorExecutor.
+			AssertGeneratedCodeMatches(
+				sourceCode: sourceCode,
+				expectedGeneratedCode: expectedGeneratedCode
+			);
 	}
 
 }
---
+++ Source/Lib/Morris.RoslynjectTests/SourceGeneratorExecutor.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis;
+using Microsoft.Extensions.DependencyInjection;
+using Morris.Roslynject.Generator;
+
+namespace Morris.RoslynjectTests;
+internal static class SourceGeneratorExecutor
+{
+	private static readonly MetadataReference MorrisRoslynjectMetadataReference =
+		MetadataReference
+		.CreateFromFile(
+			typeof(Morris.Roslynject.RegisterClassesDescendedFromAttribute)
+			.Assembly
+			.Location
+		);
+
+	private static readonly MetadataReference MSDependencyInjectionMetadataReference =
+		MetadataReference
+		.CreateFromFile(
+			typeof(ServiceLifetime)
+			.Assembly
+			.Location
+		);
+
+	public static string AssertGeneratedCodeMatches(
+		string sourceCode,
+		string expectedGeneratedCode)
+	{
+		var syntaxTree = CSharpSyntaxTree.ParseText(sourceCode);
+
+		var compilation = CSharpCompilation.Create(
+			assemblyName: "Test",
+			syntaxTrees: [syntaxTree],
+			references: Basic.Reference.Assemblies.Net80.References
+				.All
+				.Union([MorrisRoslynjectMetadataReference, MSDependencyInjectionMetadataReference]),
+			options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary)
+		);
+
+		var subject = new SourceGenerator();
+		ISourceGenerator sourceGenerator = subject.AsSourceGenerator();
+		var driver = CSharpGeneratorDriver
+			.Create(sourceGenerator)
+			.RunGenerators(compilation);
+
+		GeneratorDriverRunResult runResult = driver.GetRunResult();
+		GeneratorRunResult result = runResult.Results.Single();
+
+		GeneratedSourceResult generatedSource = result.GeneratedSources.Single();
+		Assert.AreEqual("Morris.Roslynject.g.cs", generatedSource.HintName);
+		string generatedCode = generatedSource.SyntaxTree.ToString().Replace("\r", "");
+		Assert.AreEqual(expectedGeneratedCode.Replace("\r", ""), generatedCode);
+
+		return generatedCode;
+	}
+}
