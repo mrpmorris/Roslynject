fcd1b3d1a2f0723cf2220a09ee251dd8d4b76d2a
Peter Morris <mrpmorris@gmail.com>
2024-11-15 16:10:36 +0000

    WIP

---
+++ Source/Lib/Morris.Roslynject/StaticResourcesSourceGenerator.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Morris.Roslynject;
+
+[Generator]
+public class StaticResourcesSourceGenerator : IIncrementalGenerator
+{
+	public void Initialize(IncrementalGeneratorInitializationContext context)
+	{
+		context.RegisterPostInitializationOutput(ctx =>
+		{
+			const string SourceCode =
+			$$$"""
+			using Microsoft.Extensions.DependencyInjection;
+			using System.Diagnostics.CodeAnalysis;
+
+			namespace Morris.Roslynject
+			{
+			}
+			""";
+			ctx.AddSource("Morris.Roslynject.StaticResources.g.cs", SourceCode);
+		});
+	}
+}
