d8a5d68a18dde654f94a21d661a39b325a178dc7
Peter Morris <mrpmorris@gmail.com>
2024-11-15 13:31:00 +0000

    Regex

--- Source/Lib/Morris.RoslynjectTests/RegisterClassesDescendedFromAttributeTests/GenericSearchCriteriaTests/WhenRegisteringBaseOrClosedGenericClass.cs
+++ Source/Lib/Morris.RoslynjectTests/RegisterClassesDescendedFromAttributeTests/GenericSearchCriteriaTests/WhenRegisteringBaseOrClosedGenericClass.cs
@@ -55,64 +55,64 @@ public class WhenRegisteringBaseOrClosedGenericClass
 			);
 	}
 
-	//[TestMethod]
-	//public void ThenRegistersBaseClassForEachClassMatchingRegex()
-	//{
-	//	const string sourceCode =
-	//		"""
-	//		namespace MyNamespace
-	//		{
-	//			using Microsoft.Extensions.DependencyInjection;
-	//			using Morris.Roslynject;
+	[TestMethod]
+	public void ThenRegistersBaseClassForEachClassMatchingRegex()
+	{
+		const string sourceCode =
+			"""
+			namespace MyNamespace
+			{
+				using Microsoft.Extensions.DependencyInjection;
+				using Morris.Roslynject;
 
-	//			[RegisterClassesDescendedFrom(typeof(BaseClass), ServiceLifetime.Scoped, RegisterClassAs.BaseOrClosedGenericClass, ClassRegex=@"^MyOtherNamespace\.")]
-	//			internal class MyModule : RoslynjectModule
-	//			{
-	//			}
+				[RegisterClassesDescendedFrom(typeof(BaseClass<,>), ServiceLifetime.Scoped, RegisterClassAs.BaseOrClosedGenericClass, ClassRegex=@"^MyOtherNamespace\.")]
+				internal class MyModule : RoslynjectModule
+				{
+				}
 
-	//			public abstract class BaseClass {}
-	//			public class Child1<T> : BaseClass {}
-	//			public class Child2<T> : BaseClass {}
-	//			public class Child1Child1 : Child1<int> {}
-	//		}
+				public class BaseClass<TRequest, TResponse> {}
+				public class Child1 : BaseClass<int, bool> {}
+				public class Child2 : BaseClass<string, bool> {}
+				public class Child1Child1 : Child1 {}
+			}
 
-	//		namespace MyOtherNamespace
-	//		{
-	//			using Microsoft.Extensions.DependencyInjection;
-	//			using Morris.Roslynject;
-	//			using MyNamespace;
+			namespace MyOtherNamespace
+			{
+				using Microsoft.Extensions.DependencyInjection;
+				using Morris.Roslynject;
+				using MyNamespace;
 
-	//			public class Child1Child1Child1 : Child1Child1 {}
-	//		}
-	//		""";
+				public class Child1Child1Child1 : Child1Child1 {}
+			}
+			""";
 
-	//	const string expectedGeneratedCode =
-	//		"""
-	//		using Microsoft.Extensions.DependencyInjection;
+		const string expectedGeneratedCode =
+			"""
+			using Microsoft.Extensions.DependencyInjection;
 
-	//		namespace MyNamespace
-	//		{
-	//			partial class MyModule
-	//			{
-	//				static partial void AfterRegister(IServiceCollection services);
+			namespace MyNamespace
+			{
+				partial class MyModule
+				{
+					static partial void AfterRegister(IServiceCollection services);
 
-	//				public static void Register(IServiceCollection services)
-	//				{
-	//					// RegisterClassesDescendedFrom(typeof(BaseClass), ServiceLifetime.Scoped, RegisterClassAs.BaseOrClosedGenericClass, ClassRegex=@"^MyOtherNamespace\.")
-	//					services.AddScoped(typeof(global::MyNamespace.BaseClass), typeof(global::MyOtherNamespace.Child1Child1Child1));
+					public static void Register(IServiceCollection services)
+					{
+						// RegisterClassesDescendedFrom(typeof(BaseClass<,>), ServiceLifetime.Scoped, RegisterClassAs.BaseOrClosedGenericClass, ClassRegex=@"^MyOtherNamespace\.")
+						services.AddScoped(typeof(global::MyNamespace.BaseClass<int, bool>), typeof(global::MyOtherNamespace.Child1Child1Child1));
 
-	//					AfterRegister(services);
-	//				}
-	//			}
-	//		}
+						AfterRegister(services);
+					}
+				}
+			}
 
-	//		""";
+			""";
 
-	//	SourceGeneratorExecutor
-	//		.AssertGeneratedCodeMatches(
-	//			sourceCode: sourceCode,
-	//			expectedGeneratedCode: expectedGeneratedCode
-	//		);
-	//}
+		SourceGeneratorExecutor
+			.AssertGeneratedCodeMatches(
+				sourceCode: sourceCode,
+				expectedGeneratedCode: expectedGeneratedCode
+			);
+	}
 
 }
