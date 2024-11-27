533e132245904f9cf618b8bd6360a90a2ecf2311
Peter Morris <mrpmorris@gmail.com>
2024-11-27 15:44:35 +0000

    Remove unneeded parser classes

--- Source/Lib/Morris.Roslynject/StaticResources/RegisterClassAs.cs
+++ Source/Lib/Morris.Roslynject/StaticResources/RegisterClassAs.cs
@@ -9,18 +9,6 @@ internal enum RegisterClassAs
 	BaseClosedGenericClass
 }
 
-internal static class RegisterClassAsParser
-{
-	[MethodImpl(MethodImplOptions.AggressiveInlining)]
-	public static object? Parse(string value) =>
-		value switch {
-			nameof(RegisterClassAs.DescendantClass) => RegisterClassAs.DescendantClass,
-			nameof(RegisterClassAs.BaseClass) => RegisterClassAs.BaseClass,
-			nameof(RegisterClassAs.BaseClosedGenericClass) => RegisterClassAs.BaseClosedGenericClass,
-			_ => null
-		};
-}
-
 internal static partial class SourceCode
 {
 	public const string RegisterClassAs =
--- Source/Lib/Morris.Roslynject/StaticResources/RegisterInterfaceAs.cs
+++ Source/Lib/Morris.Roslynject/StaticResources/RegisterInterfaceAs.cs
@@ -9,18 +9,6 @@ internal enum RegisterInterfaceAs
 	BaseClosedGenericInterface
 }
 
-internal static class RegisterInterfaceAsParser
-{
-	[MethodImpl(MethodImplOptions.AggressiveInlining)]
-	public static object? Parse(string value) =>
-		value switch {
-			nameof(RegisterInterfaceAs.ImplementedInterface) => RegisterInterfaceAs.ImplementedInterface,
-			nameof(RegisterInterfaceAs.BaseInterface) => RegisterInterfaceAs.BaseInterface,
-			nameof(RegisterInterfaceAs.BaseClosedGenericInterface) => RegisterInterfaceAs.BaseClosedGenericInterface,
-			_ => null
-		};
-}
-
 internal static partial class SourceCode
 {
 	public const string RegisterInterfaceAs =
