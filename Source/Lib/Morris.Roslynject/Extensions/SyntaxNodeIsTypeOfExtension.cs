b3fdcf54b477b6de81661d4fc7b6024a9fd1d1e1
Peter Morris <mrpmorris@gmail.com>
2024-12-03 15:28:43 +0000

    MissingRoslynjectModuleAttribute analyzer

---
+++ Source/Lib/Morris.Roslynject/Extensions/SyntaxNodeIsTypeOfExtension.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis;
+
+namespace Morris.Roslynject.Extensions;
+
+internal static class SyntaxNodeIsTypeOfExtension
+{
+	public static bool IsTypeOf(
+		this SyntaxNode node,
+		SemanticModel semanticModel,
+		string attributeFullName)
+	=>
+		semanticModel.GetSymbolInfo(node).Symbol is IMethodSymbol methodSymbol
+		&& methodSymbol.ContainingType.ToDisplayString() == attributeFullName;
+}
