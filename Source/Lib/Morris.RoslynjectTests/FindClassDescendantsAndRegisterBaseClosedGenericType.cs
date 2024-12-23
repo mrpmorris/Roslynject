8fd7694c16681417ef463a0da560f7efb4c468b5
Peter Morris <mrpmorris@gmail.com>
2024-12-23 10:54:04 +0000

    Additional unit test

--- Source/Lib/Morris.RoslynjectTests/FindClassDescendantsAndRegisterBaseClosedGenericType.cs
+++ Source/Lib/Morris.RoslynjectTests/FindClassDescendantsAndRegisterBaseClosedGenericType.cs
@@ -48,6 +48,51 @@ public class FindClassDescendantsAndRegisterBaseClosedGenericType
 	}
 
 	[TestMethod]
+	public void WhenBaseTypeIsNotGeneric_ThenRegistersNonGenericBaseType()
+	{
+		string sourceCode =
+			$$"""
+			using Morris.Roslynject;
+			namespace Tests
+			{
+				public class BaseClass { }
+				public class Child1<T> : BaseClass { }
+				public class Child1GrandChild1 : Child1<int> { }
+
+				[Roslynject(Find.DescendantsOf, typeof(BaseClass), Register.BaseClosedGenericType, WithLifetime.Scoped)]
+				public partial class MyModule {}
+			}
+			""";
+
+		string expectedGeneratedCode =
+			$$"""
+			using Microsoft.Extensions.DependencyInjection;
+			namespace Tests
+			{
+				partial class MyModule
+				{
+					static partial void AfterRegisterServices(IServiceCollection services);
+
+					public static void RegisterServices(IServiceCollection services)
+					{
+						//Find DescendantsOf, Type Tests.BaseClass, Register BaseClosedGenericType, WithLifetime Scoped
+						services.AddScoped(typeof(global::Tests.BaseClass), typeof(global::Tests.Child1GrandChild1));
+
+						AfterRegisterServices(services);
+					}
+				}
+			}
+			""";
+
+		SourceGeneratorExecutor
+			.AssertGeneratedCodeMatches(
+				sourceCode: sourceCode,
+				expectedGeneratedCode: expectedGeneratedCode
+			);
+	}
+
+
+	[TestMethod]
 	public void WhenCandidateIsAbstract_ThenDoesNotRegister()
 	{
 		string sourceCode =
